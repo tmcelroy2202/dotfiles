6935bf30256f6c05afb4b6210450ecd4121a6140
tmcelro2202 <tommy.a.mcelroy@gmail.com>
2024-05-27 22:34:19 -0400

    try again

--- home.nix
+++ home.nix
@@ -489,11 +489,11 @@ programs.waybar = {
   home.file.".config/xournalpp/settings.xml".source = config.lib.file.mkOutOfStoreSymlink /home/tommy/.dotfiles/xournal/settings.xml;
   home.file.".cargo/bin/mdpls".source = config.lib.file.mkOutOfStoreSymlink /home/tommy/.dotfiles/mdpls/mdpls;
 
-  programs.git = {
-    enable = true;
-    userEmail = "tommy.a.mcelroy@gmail.com";
-    userName = "tmcelro2202";
-  };
+  # programs.git = {
+  #   enable = true;
+  #   userEmail = "tommy.a.mcelroy@gmail.com";
+  #   userName = "tmcelro2202";
+  # };
 
 
   programs.rbw = {
